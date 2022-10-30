4afd6139f38200bbdc6037e5fa9305df3a9aaa14
Desmond <desmondmies@gmail.com>
2022-10-30 19:47:24 +0100

    adding mongodb realm support WIP
    errors in CpArticles.vue file

--- src/views/CpArticles.vue
+++ src/views/CpArticles.vue
@@ -1,13 +1,43 @@
 <template>
   articlessssss
+
+  <ul>
+    <li
+      v-for="article in articles"
+      :key="article._id"
+    >
+      {{article.title}}
+    </li>
+  </ul>
+
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IArticle } from '@/models/article.interface';
+import { app, credentials } from "@/utils/mongo.client";
+
+export default defineComponent({
   name: 'CpArticles',
   components: {
-  }
-}
+  },
+
+  data: () => ({ articles: [] as IArticle[] }), //ERREUR ICI
+
+  methods: {
+    async getListOfArticles() {
+      const user: Realm.User = await app.logIn(credentials);
+      const listOfArticle: Promise<IArticle[]> = user.functions.getAllArticles();
+      listOfArticle.then((resp) => {
+        this.articles = resp;
+      });
+    },
+  },
+
+  mounted() {
+    this.getAllArticles();
+  },
+});
 </script>
 
 <style>
