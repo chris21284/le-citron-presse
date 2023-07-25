a7fedd9a44c4b93935a6528e77ff95a2d67e64d1
Desmond <desmondmies@gmail.com>
2023-07-25 04:02:29 +0200

    nav menu mobile wip (working but need testing and responsive)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import { createApp, toRef } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/css/custom-fonts.css'
@@ -7,6 +7,38 @@ import { createPinia } from 'pinia';
 const app = createApp(App);
 
 const pinia = createPinia();
+
+const ClosableDirective = {
+    mounted(el, binding) {
+        const handleOutsideClick = (e) => {
+            e.stopPropagation();
+            const { handler, exclude } = binding.value;
+
+            let clickedOnExcludedEl = false;
+            exclude.forEach((refName) => {
+                if (!clickedOnExcludedEl) {
+                    const excludedEl = toRef(refName);
+                    clickedOnExcludedEl = e.target.outerHTML.includes(excludedEl.value)
+                }
+            });
+
+            if (!el.contains(e.target) && !clickedOnExcludedEl) {
+                binding.instance[handler]();
+            }
+        };
+
+        window.addEventListener('click', handleOutsideClick);
+        window.addEventListener('touchstart', handleOutsideClick);
+
+        el._handleOutsideClick = handleOutsideClick
+    },
+    beforeUnmount(el) {
+        window.removeEventListener('click', el._handleOutsideClick);
+        window.removeEventListener('touchstart', el._handleOutsideClick);
+    },    
+};
+
+app.directive('outside-closable', ClosableDirective);
 
 app.use(pinia);
 app.use(router);
--- src/views/v2/HeaderComponent.vue
+++ src/views/v2/HeaderComponent.vue
@@ -6,18 +6,28 @@
         </router-link>
 
         <nav v-if="isShrinked">
-            <router-link to="/"><span class="link">ACCUEIL</span></router-link>
-            <router-link to="/macarons"><span class="link">MACARONS</span></router-link>
-            <router-link to="/patisserie"><span class="link">PÂTISSERIE</span></router-link>
-            <router-link to="/biscuits"><span class="link">BISCUITS</span></router-link>
-            <router-link to="/a-propos-de-nous"><span class="link">À PROPOS DE NOUS</span></router-link>
-            <router-link to="/contact"><span class="link">CONTACT</span></router-link>
+            <router-link :to="homePath"><span class="link">ACCUEIL</span></router-link>
+            <router-link :to="macaronPath"><span class="link">MACARONS</span></router-link>
+            <router-link :to="patisseriePath"><span class="link">PÂTISSERIE</span></router-link>
+            <router-link :to="patisseriePath"><span class="link">BISCUITS</span></router-link>
+            <router-link :to="aProposPath"><span class="link">À PROPOS DE NOUS</span></router-link>
+            <router-link :to="contactPath"><span class="link">CONTACT</span></router-link>
         </nav>
 
         <div class="nav-hamburger">
-            <!-- icon top right trigger appearing of nav menu left panel max width -->
-            <div class="hamburger-icon"></div>
-            <div class="nav-panel"></div>
+            <div ref="hamburger_icon" class="hamburger_icon" :class="isHamburgerNavActive ? 'hamburger-active': ''" @click="onClickHamburgerNav">
+                <span class="hamburger_layer"></span>
+                <span class="hamburger_layer"></span>
+                <span class="hamburger_layer"></span>
+            </div>
+            <div v-outside-closable="{ exclude: ['hamburger_icon', 'hamburger_layer'], handler: 'disableHamburgerNav' }" class="nav-panel" :class="isHamburgerNavActive ? 'nav-active': ''">
+                <router-link :to="homePath"><span class="link">ACCUEIL</span></router-link>
+                <router-link :to="macaronPath"><span class="link">MACARONS</span></router-link>
+                <router-link :to="patisseriePath"><span class="link">PÂTISSERIE</span></router-link>
+                <router-link :to="patisseriePath"><span class="link">BISCUITS</span></router-link>
+                <router-link :to="aProposPath"><span class="link">À PROPOS DE NOUS</span></router-link>
+                <router-link :to="contactPath"><span class="link">CONTACT</span></router-link>
+            </div>
         </div>
 
         <div class="cart-container">
@@ -49,13 +59,28 @@
                 type: String,
                 default: ''
             }
+        },
+        data() {
+            return {
+                homePath: "/",
+                macaronPath: "/macarons",
+                patisseriePath: "/patisserie",
+                biscuitPath: "/biscuits",
+                aProposPath: "/a-propos-de-nous",
+                contactPath: "/contact",
+                isHamburgerNavActive: false
+            }
+        },
+        methods: {
+            onClickHamburgerNav() { this.isHamburgerNavActive = !this.isHamburgerNavActive; },
+            disableHamburgerNav() { this.isHamburgerNavActive = false; }
         }
     }
 </script>
 
 <style>
     header {
-        /* display: flex; */
+        display: flex;
         position: fixed;
         width: 100vw;
         min-height: 7.5rem;
@@ -75,28 +100,14 @@
         flex-wrap: nowrap;
         justify-content: flex-end;
         align-items: center;
-
+        width: 100%;
         margin-right: 2rem;
         gap: 0.2rem;
         animation: appear 0.3s normal forwards ease-in-out;
     }
 
-    .nav-hamburger {
-        position: relative;
-    }
-
-    @keyframes appear {
-        0% {
-            opacity: 0;
-            transform: translateX(2%);
-        }
-        100% {
-            opacity: 1;
-            transform: translateX(0%);
-        }
-    }
-
-    nav a {
+    nav a,
+    .nav-panel a {
         text-decoration: none;
         font-family: ps-bold, Avenir, Helvetica, Arial, sans-serif;
         color: var(--white);
@@ -104,6 +115,91 @@
         display: flex;
         align-items: center;
         font-size: 1rem;
+    }
+
+    nav a.router-link-exact-active,
+    .nav-panel a.router-link-exact-active {
+        color: var(--green);
+    }
+    nav a.router-link-exact-active .link,
+    .nav-panel a.router-link-exact-active .link {
+        background-color: var(--yellow);
+    }
+    nav a.router-link-exact-active:hover,
+    .nav-panel a.router-link-exact-active:hover {
+        color: var(--light-green);
+    }
+
+    .nav-hamburger {
+        position: relative;
+        min-height: 100%;
+        width: 100%;
+        display: none;
+        justify-content: flex-end;
+        align-items: center;
+        margin-right: 3rem;
+    }
+
+    .hamburger_icon {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+        max-height: 2rem;
+        align-items: flex-end;
+        justify-content: center;
+        padding: 1rem;
+        cursor: pointer;
+    }
+    .hamburger_icon:hover {
+        background-color: var(--lighter-green);
+        border-radius: 50%;
+    }
+    .hamburger_icon:hover .hamburger_layer { background-color: var(--green); }
+
+    .hamburger_layer {
+        background-color: var(--white);
+        min-height: 0.3rem;
+        min-width: 2.3rem;
+        border-radius: 1rem;
+        transform-origin: 0;
+        opacity: 1;
+        transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
+                    opacity 0.55s ease;
+    }
+
+    .hamburger-active { max-width: 2rem; }
+
+    .hamburger-active > .hamburger_layer:nth-child(1) {
+        transform: rotate(45deg) translate(15%, -100%);
+        transform-origin: 0;
+    }
+    .hamburger-active > .hamburger_layer:nth-child(2) {
+        opacity: 0;
+        transform: scaleX(0);
+    }
+    .hamburger-active > .hamburger_layer:nth-child(3) {
+        transform: rotate(-45deg) translate(10%, 130%);
+        transform-origin: 0;
+    }
+
+    .nav-panel {
+        position: absolute;
+        display: none;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        left: 0;
+        top: 100%;
+        width: 100%;
+        max-width: 20rem;
+        min-height: 30rem;
+        background-color: var(--light-green);
+        border-radius: 0 0 30px 0;
+        animation: appear-mobile 0.3s normal forwards ease-in-out;
+    }
+
+    .nav-active {
+        display: flex;
     }
 
     .link {
@@ -116,16 +212,6 @@
     .link:hover {
         background-color: var(--lighter-green);
         cursor: pointer;
-    }
-
-    nav a.router-link-exact-active {
-        color: var(--green);
-    }
-    nav a.router-link-exact-active .link {
-        background-color: var(--yellow);
-    }
-    nav a.router-link-exact-active:hover {
-        color: var(--light-green);
     }
 
     .logo {
@@ -194,6 +280,28 @@
         cursor: pointer;
     }
 
+    /* ANIMATIONS */
+    @keyframes appear {
+        0% {
+            opacity: 0;
+            transform: translateX(2%);
+        }
+        100% {
+            opacity: 1;
+            transform: translateX(0%);
+        }
+    }
+
+    @keyframes appear-mobile {
+        0% {
+            transform: translateX(-100%);
+        }
+        100% {
+            transform: none;
+        }
+    }
+
+    /* MEDIA QUERIES */
     @media only screen and (max-width: 1200px) {
         header { min-height: 7rem; }
         .logo { max-width: 14rem; }
@@ -210,6 +318,11 @@
             max-width: 9rem;
             left: 13.5rem; 
         }
+    }
+
+    @media only screen and (max-width: 950px) {
+        nav { display: none; }
+        .nav-hamburger { display: flex; }
     }
 
     @media only screen and (max-width: 570px) {
